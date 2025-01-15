c0eee999f9173ac54d5bb17809e7928cb64a243b
thiagociarlini <61710629+thiagociarlini@users.noreply.github.com>
2025-01-15 10:44:49 -0300

    Primeira versão

---
+++ script.js
@@ -0,0 +1,18 @@
+
+document.getElementById('login-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  if (username === 'admin' && password === '1234') {
+    alert('Acesso autorizado');
+    window.location.href = 'dashboard.html';
+  } else {
+    alert('Usuário ou senha incorretos');
+  }
+});
+
+function logout() {
+  alert('Você saiu do sistema.');
+  window.location.href = 'index.html';
+}
